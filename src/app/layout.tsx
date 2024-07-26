697dbc03754b605ed56be1654eb0b30a58ec0ef0
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-07-26 23:49:04 +0200

    fix: hydration bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
-import "@/styles/globals.css";
 
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
+import GridPattern from "@/components/magicui/grid-pattern";
+import "@/styles/globals.css";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -20,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
@@ -34,6 +35,15 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <GridPattern
+            width={40}
+            height={40}
+            x={0}
+            y={0}
+            className={cn(
+              "[mask-image:radial-gradient(800px_circle_at_top,white,transparent)]"
+            )}
+          />
         </ThemeProvider>
       </body>
     </html>
