681a5ae635ae9b0c81d404431423dee5ef09ff55
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-07-26 23:48:49 +0200

    feat: add image

--- src/app/(sections)/layout.tsx
+++ src/app/(sections)/layout.tsx
@@ -2,15 +2,23 @@
 
 import ButtonShapeTabs from "@/components/button-tabs";
 import Footer from "@/components/footer";
+import Image from "next/image";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen max-w-5xl mx-auto text-center">
-      <header className="p-8">
-        <h1 className="text-5xl font-bold pt-8 tracking-tighter">
+    <div className="min-h-screen max-w-5xl mx-auto">
+      <header className="p-8 flex items-center justify-center flex-col text-center">
+        <h1 className="text-5xl font-bold pt-8 tracking-tighter z-10">
           Karol Rynkowski
         </h1>
-        <p className="text-lg text-muted-foreground">Engineer & Designer</p>
+        <p className="text-lg text-muted-foreground">Software Developer</p>
+        <Image
+          src="/vercel.svg"
+          alt="photo"
+          width={200}
+          height={200}
+          priority
+        />
       </header>
       <ButtonShapeTabs />
       <main>{children}</main>
