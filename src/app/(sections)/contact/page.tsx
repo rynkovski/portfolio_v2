e658d8c6fb859996e1433ff0e01237408cdbc3c7
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-08-09 14:22:48 +0200

    feat: add contact links

--- src/app/(sections)/contact/page.tsx
+++ src/app/(sections)/contact/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface NavigationItem {
   name: string;
   href: string;
@@ -10,7 +12,7 @@ const navigation: {
   social: [
     {
       name: "Twitter",
-      href: "https://twitter.com/ansub",
+      href: "https://twitter.com/rynkovski",
       icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
@@ -19,7 +21,7 @@ const navigation: {
     },
     {
       name: "GitHub",
-      href: "https://github.com/ansub",
+      href: "https://github.com/rynkovski",
       icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -32,7 +34,7 @@ const navigation: {
     },
     {
       name: "LinkedIn",
-      href: "https://linkedin.com/in/ansub",
+      href: "https://pl.linkedin.com/in/karol-rynkowski-050022263?original_referer=https://www.google.com/",
       icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -48,19 +50,21 @@ const navigation: {
 function Index() {
   return (
     <section id="contact">
-      <div className="mx-auto w-full max-w-6xl px-4 py-12 sm:px-6 lg:px-8">
-        <div className="mt-5 flex justify-center space-x-6">
+      <div className="w-full max-w-6xl px-4 py-12 mx-auto sm:px-6 lg:px-8">
+        <div className="flex justify-center gap-10 mt-5">
           {navigation.social.map((item) => (
-            <a
+            <Link
               key={item.name}
               href={item.href}
               className="text-gray-600 hover:text-primary dark:hover:text-primary"
+              rel="noopener noreferrer"
+              target="_blank"
             >
               <span className="sr-only">{item.name}</span>
               {item.icon && (
-                <item.icon className="h-5 w-5" aria-hidden="true" />
+                <item.icon className="w-10 h-10" aria-hidden="true" />
               )}
-            </a>
+            </Link>
           ))}
         </div>
       </div>
