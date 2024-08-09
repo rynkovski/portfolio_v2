4c00f868d6dbe4ce4e25cae5ea2062152477a8dc
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-08-09 14:23:05 +0200

    feat: change footer text

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,14 +5,13 @@ import { Button } from "./ui/button";
 const Footer: React.FC = () => {
   return (
     <footer className="w-full">
-      <p className="mt-6 text-center text-sm text-gray-600 dark:text-gray-100">
+      <p className="my-6 text-center text-sm text-gray-600 dark:text-gray-100">
         &copy; {new Date().getFullYear()}
         <Link href="/">
           <Button size={"sm"} variant={"link"}>
             rynkovski
           </Button>
         </Link>
-        All rights reserved.
       </p>
     </footer>
   );
