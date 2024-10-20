67af20a529e6d8c81852469fbe557046817b04ed
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-10-20 21:52:59 +0200

    feat: update skills section

--- src/app/(sections)/page.tsx
+++ src/app/(sections)/page.tsx
@@ -1,63 +1,16 @@
-import IconCloud from "@/components/magicui/icon-cloud";
+import AboutSection from "@/components/about-section";
+import FadeUp from "@/components/fadeup";
+import SkillsSection from "@/components/skills-section";
 
-const slugs = [
-  "typescript",
-  "javascript",
-  "tailwindcss",
-  "chakraui",
-  "shadcnui",
-  "react",
-  "html5",
-  "css3",
-  "markdown",
-  "nextdotjs",
-  "postgresql",
-  "firebase",
-  "reactquery",
-  "reactrouter",
-  "zod",
-  "vite",
-  "supabase",
-  "vercel",
-  "mui",
-  "cypress",
-  "git",
-  "github",
-  "gitlab",
-  "visualstudiocode",
-  "adobephotoshop",
-  "figma",
-  "playwright",
-];
 export default function Index() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <section className="text-muted-foreground flex gap-2 flex-col mx-4">
-        <p>
-          Hey, I&apos;m Karol Rynkowski, a{" "}
-          <span className="text-white">frontend developer </span> who loves
-          making cool stuff online. I&apos;m always{" "}
-          <span className="text-white">learning</span> about new tech and ways
-          to make things <span className="text-white">better</span>, from the
-          basics to the latest tools.
-        </p>
-        <p>
-          When I&apos;m not coding, I enjoy{" "}
-          <span className="text-white">exploring</span> new places, keeping up
-          with tech news, and hitting the gym.
-        </p>
-        <p>
-          If you&apos;re <span className="text-white">interested</span> in
-          working together, I&apos;m <span className="text-white">ready</span>{" "}
-          to take on challenges and bring fresh ideas to the table.
-        </p>
-      </section>
-      <section className="text-center">
-        <p className="text-3xl font-mono mb-2 mt-4">Skills</p>
-        <div className="relative flex h-full w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg border bg-background px-20 pb-20 pt-8 ">
-          <IconCloud iconSlugs={slugs} />
-        </div>
-      </section>
+      <FadeUp delay={0.2} duration={0.8}>
+        <AboutSection />
+      </FadeUp>
+      <FadeUp delay={0.4} duration={0.8}>
+        <SkillsSection />
+      </FadeUp>
     </div>
   );
 }
