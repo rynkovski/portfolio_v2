12af95a0ce91deb09eae0f27d702f0258a9abb18
rynkovski <118986014+rynkovski@users.noreply.github.com>
2025-03-09 22:33:15 +0100

    feat: update text content

--- src/components/about-section.tsx
+++ src/components/about-section.tsx
@@ -2,18 +2,11 @@ function AboutSection() {
   return (
     <section id="about" className="flex gap-2 flex-col -mb-3 mt-24">
       <span className="font-medium mb-5 sm:mb-6">About</span>
-      <p className="text-muted-foreground/90">
-        Hey, I&apos;m Karol Rynkowski, a software developer who loves making
-        cool stuff online. I&apos;m always learning about new tech and ways to
-        make things better, from the basics to the latest tools.
-      </p>
-      <p className="text-muted-foreground/90">
-        When I&apos;m not coding, I enjoy exploring new places, keeping up with
-        tech news, and hitting the gym.
-      </p>
-      <p className="text-muted-foreground/90">
-        If you&apos;re interested in working together, I&apos;m ready to take on
-        challenges and bring fresh ideas to the table.
+      <p className="text-muted-foreground">
+        Hey, I&apos;m Karol Rynkowski, a software developer who likes to build
+        scalable web and mobile applications with a focus on performance
+        optimization and accessibility. When not coding, I brew specialty coffee
+        and explore photography.
       </p>
     </section>
   );
--- src/components/blog-section.tsx
+++ src/components/blog-section.tsx
@@ -3,7 +3,7 @@ function BlogSection() {
     <section id="blog" className="-mb-3 mt-16 sm:mt-32">
       <span className="font-medium mb-5 sm:mb-6">Blog</span>
       <p className="text-muted-foreground">
-        {"Coming soon... first I need to know what to write about :)"}{" "}
+        {"Building thoughtful content..."}{" "}
       </p>
     </section>
   );
