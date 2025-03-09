00292a94e3933cceb9143f2df4012cb0c09cf3d5
rynkovski <118986014+rynkovski@users.noreply.github.com>
2025-03-09 22:53:11 +0100

    feat: update about

--- src/components/about-section.tsx
+++ src/components/about-section.tsx
@@ -3,10 +3,11 @@ function AboutSection() {
     <section id="about" className="flex gap-2 flex-col -mb-3 mt-24">
       <span className="font-medium mb-5 sm:mb-6">About</span>
       <p className="text-muted-foreground">
-        Hey, I&apos;m Karol Rynkowski, a software developer who likes to build
-        scalable web and mobile applications with a focus on performance
-        optimization and accessibility. When not coding, I brew specialty coffee
-        and explore photography.
+        Hey! I&apos;m Karol Rynkowski, a software developer who likes to build
+        scalable web and mobile applications with a focus on performance and
+        accessibility. When I&apos;m not coding, I love exploring new places
+        with my dog. Let&apos;s get in touch if you'd like to work on something
+        together!
       </p>
     </section>
   );
